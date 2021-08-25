10213f68c61306d7d59068e544d717d0c24e9721
charity.rogers <rogerscl116@gmail.com>
2021-08-24 20:31:47 -0500

    initialize and sync the state, submit the form data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,33 +1,35 @@
 import React, { useState } from 'react';
 
-const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
     const { name, email, message } = formState;
 
     function handleChange(e) {
-        setFormState({...formState, name: e.target.value })
+      setFormState({...formState, [e.target.name]: e.target.value })
       }
-      
+
+    function handleSubmit(e) {
+      e.preventDefault();
       console.log(formState);
+      }
 
     // JSX
     return (
         <section>
           <h1>Contact me</h1>
-          <form id="contact-form">
+          <form id="contact-form" onSubmit={handleSubmit}>
             <div>
             <label htmlFor="name">Name:</label>
             <input type="text" defaultValue={name} onChange={handleChange} name="name" />
             </div>
             <div>
             <label htmlFor="email">Email address:</label>
-            <input type="email" name="email" defaultValue={email} />
+            <input type="email" name="email" defaultValue={email} onChange={handleChange} />
             </div>
             <div>
             <label htmlFor="message">Message:</label>
-            <textarea name="message" defaultValue={message} rows="5" />
+            <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
             </div>
             <button type="submit">Submit</button>
           </form>
